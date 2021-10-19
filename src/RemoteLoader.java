3b401203d35cacab2fc99b52af7362b715f9f12c
khoivudev <vuhuykhoi1997@gmail.com>
2021-10-19 22:12:31 +0900

    [feat] Implement test for Ceiling Fan

--- src/RemoteLoader.java
+++ src/RemoteLoader.java
@@ -71,21 +71,19 @@ public class RemoteLoader {
         CeilingFan ceilingFan = new CeilingFan("Living Room");
 
         CeilingFanOffCommand ceilingFanOff = new CeilingFanOffCommand(ceilingFan);
-        CeilingFanLowCommand ceilingFanLow = new CeilingFanLowCommand(ceilingFan);
         CeilingFanMediumCommand ceilingFanMedium = new CeilingFanMediumCommand(ceilingFan);
         CeilingFanHighCommand ceilingFanHigh = new CeilingFanHighCommand(ceilingFan);
 
-        remoteControl.setCommand(0, ceilingFanLow, ceilingFanOff);
-        remoteControl.setCommand(1, ceilingFanMedium, ceilingFanOff);
-        remoteControl.setCommand(2, ceilingFanHigh, ceilingFanOff);
+        remoteControl.setCommand(0, ceilingFanMedium, ceilingFanOff);
+        remoteControl.setCommand(1, ceilingFanHigh, ceilingFanOff);
         
         remoteControl.onButtonWasPushed(0);
+        remoteControl.offButtonWasPushed(0);
+        System.out.println(remoteControl);
+        remoteControl.undoButtonWasPushed();
+
         remoteControl.onButtonWasPushed(1);
-        remoteControl.onButtonWasPushed(2);
-        remoteControl.offButtonWasPushed(2);
-        remoteControl.undoButtonWasPushed();
-        remoteControl.undoButtonWasPushed();
-        remoteControl.undoButtonWasPushed();
+        System.out.println(remoteControl);
         remoteControl.undoButtonWasPushed();
     }
 }
